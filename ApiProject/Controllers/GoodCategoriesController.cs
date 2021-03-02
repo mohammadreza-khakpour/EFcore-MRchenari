7089ffa93573bc3fcccb4e9d28cda00ff34cba5e
taav <rezakhakpoorboraz@gmail.com>
2021-03-02 05:23:20 -0800

    3shanbe 12esfand99 16:53

--- ApiProject/Controllers/GoodCategoriesController.cs
+++ ApiProject/Controllers/GoodCategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using ApiProject.Models;
+using ApiProject.Models.DTOs;
 using ApiProject.Models.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -33,6 +34,39 @@ namespace ApiProject.Controllers
             };
 
             _context.GoodCategories.Add(goodCategory);
+
+            _context.SaveChanges();
+        }
+
+        [HttpGet]
+        public IList<GetGoodCategoryDto> GetAll()
+        {
+            return _context.GoodCategories.Select
+                  (_ => new GetGoodCategoryDto
+                  {
+                      Id = _.Id,
+                      Title = _.Title
+                  }).ToList();
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, UpdateGoodCategoryDto dto)
+        {
+            var category = _context.GoodCategories.Find(id);
+
+            category.Title = dto.Title;
+
+            _context.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var category = _context.GoodCategories.Find(id);
+            _context.GoodCategories.Remove(category);
+
+            /* var goodCategory = new GoodCategory { Id = id };
+             _context.Entry(goodCategory).State = EntityState.Deleted;*/
 
             _context.SaveChanges();
         }
