8c01da0d8cb9ca65de7976890add471c4e9737b9
taav <rezakhakpoorboraz@gmail.com>
2021-03-02 05:01:21 -0800

    init 3shanbe 12 esfand99 16:23

---
+++ ApiProject/Controllers/GoodCategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using ApiProject.Models;
+using ApiProject.Models.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProject.Controllers
+{
+    [ApiController]
+    [Route("api/good-categories")]
+    public class GoodCategoriesController : Controller
+    {
+        private readonly ApiDbContext _context;
+
+        public GoodCategoriesController()
+        {
+            _context = new ApiDbContext();
+        }
+
+        [HttpPost]
+        public void Add([Required][FromBody] string Title)
+        {
+            if (_context.GoodCategories.Any(_ => _.Title == Title))
+            {
+                throw new GoodCategoryTitleCantBeDuplicatedExcption();
+            }
+            var goodCategory = new GoodCategory
+            {
+                Title = Title
+            };
+
+            _context.GoodCategories.Add(goodCategory);
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ ApiProject/Models/ApiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProject.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=RezaSuperMarket;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<GoodCategory> GoodCategories { get; set; }
+        public DbSet<Good> Goods { get; set; }
+        public DbSet<GoodEntry> GoodEntries { get; set; }
+        public DbSet<SalesFactor> SalesFactors { get; set; }
+
+    }
+}
