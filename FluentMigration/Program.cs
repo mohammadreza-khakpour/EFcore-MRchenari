75356e0c5d5324b56a1160c68164187dabf01a7d
reza <rezakhakpoorboraz@gmail.com>
2021-03-03 05:44:06 -0800

    4shanbe 13esfand99 17:13

--- FluentMigration/Program.cs
+++ FluentMigration/Program.cs
@@ -26,7 +26,7 @@ namespace FluentMigration
                 .ConfigureRunner(rb => rb
                     .AddSqlServer()
                     .WithGlobalConnectionString("Server=.;Database=FluentMigrationTest;Trusted_Connection=True;")// should create the database first manually
-                    .ScanIn(typeof(Program).Assembly).For.Migrations())
+                    .ScanIn(typeof(Program).Assembly).For.Migrations())// https://docs.microsoft.com/en-us/dotnet/api/system.reflection.assembly?view=net-5.0
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
                 .BuildServiceProvider(false);
         }
